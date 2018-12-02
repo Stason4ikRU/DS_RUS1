f7a26be640e5ff4cc67a20a3670f0ba7a4a6d8dc
Stason4ikRU <45006095+Stason4ikRU@users.noreply.github.com>
2018-12-02 13:54:19 +0300

    убрал test и поменял нумерацию версии

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
-name = "Russian test- Hamlet"
-version = "4.6.6"
+name = "Russian - Hamlet"
+version = "4.7"
 desc = {en = "Russian localization of the game compiled from the user-suggested translations. The best translations are chosen via user voting. \n\nVersion: "..version,
 		ru = "\nРусификация игры, собранная на основе\nголосований за переводы реплик.\nВерсия: "..version}
 description = desc[language] or desc["en"]
