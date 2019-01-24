ebddd995544e26024aff538a0ed82b22822386b9
Stason4ikRU <45006095+Stason4ikRU@users.noreply.github.com>
2019-01-24 19:39:13 +0300

    s

--- modinfo.lua
+++ modinfo.lua
@@ -1,5 +1,5 @@
-name = "Russian - Hamlet"
-version = "4.9.9"
+name = "Russian test - Hamlet"
+version = "5.0"
 desc = {en = "Russian localization of the game compiled from the user-suggested translations. The best translations are chosen via user voting. \n\nVersion: "..version,
 		ru = "\nРусификация игры, собранная на основе\nголосований за переводы реплик.\nВерсия: "..version}
 description = desc[language] or desc["en"]
